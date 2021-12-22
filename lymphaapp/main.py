c864e5b51d779b18767caef20e32083d4a00c528
Rickard Hultgren <sababa.sababa@gmail.com>
2021-12-22 12:54:09 +0000

    update

--- lymphaapp/main.py
+++ lymphaapp/main.py
@@ -8,7 +8,8 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
 
-x = 0
+steps= 1
+turn = 0
 sm = ScreenManager()
 
 class ScreenOne(Screen):
@@ -16,28 +17,34 @@ class ScreenOne(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         global sc1
-        global x
-        x += 1
+        global steps
+        global turn
+        if steps > 1 :
+          steps += 1
+        turn += 1
         print("hello")
-        my_box = BoxLayout()
+        my_box= BoxLayout()
         #my_show_list = ["My Way", "Wine Drinker", "Boots"]
         my_box.my_buttons = [] # if you want to keep an "easy" reference to your buttons to do something with them later
                                #kivy doesnt crashes because it creates the property automatically
         #for message in my_show_list:
-        for x in range(0,x):
+        for step in range(0,steps):
             button = Button(text="press")
             button.bind(on_press=self.changer)
             my_box.my_buttons.append(button)
             my_box.add_widget(button)
-            print(x)
+            print(step)
         self.add_widget(my_box)            
         #return my_box
 
     def changer(self,*args):
         global sc1
-        global x
-        x += 1
-        print(x)
+        global steps
+        global turn
+        if turn == 2 :
+          steps += 1
+        turn += 1
+        print(steps)
         Clock.unschedule(self.__init__())
         #self.manager.current = 'screen1'        
         
@@ -47,7 +54,6 @@ sc1 = ScreenOne(name='screen1')
 
 class TestClass(App):
     def build(self):
-        global x
         global sc1
         
         
