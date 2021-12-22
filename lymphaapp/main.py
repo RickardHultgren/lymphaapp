25ddc7de12ebace2f90818db992b154c2faa87e4
Rickard Hultgren <sababa.sababa@gmail.com>
2021-12-22 11:08:22 +0000

    update

--- lymphaapp/main.py
+++ lymphaapp/main.py
@@ -1,61 +1,66 @@
-#import kivy
-
-#from kivy.app import App
-#from kivy.uix.button import Button
-#from kivy.uix.widget import Widget
-#x = 1
-#class Example(App):
-
-#    def build(self):
-#      buttons = list()
-#      for y in range(0,x):
-#           button = Button(text="Button")
-#           buttons.append(button)
-#      return buttons
-
-#Example().run()
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import kivy
 from kivy.app import App
-from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.clock import mainthread
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang import Builder
+from kivy.clock import Clock
 
-Builder.load_string('''
-<MapScreen>:
- name: 'map'
+x = 0
+sm = ScreenManager()
 
- GridLayout:
-  id: grid
-  cols: 1
+class ScreenOne(Screen):
+    global x
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        global sc1
+        global x
+        x += 1
+        print("hello")
+        my_box = BoxLayout()
+        #my_show_list = ["My Way", "Wine Drinker", "Boots"]
+        my_box.my_buttons = [] # if you want to keep an "easy" reference to your buttons to do something with them later
+                               #kivy doesnt crashes because it creates the property automatically
+        #for message in my_show_list:
+        for x in range(0,x):
+            button = Button(text="press")
+            button.bind(on_press=self.changer)
+            my_box.my_buttons.append(button)
+            my_box.add_widget(button)
+            print(x)
+        self.add_widget(my_box)            
+        #return my_box
+
+    def changer(self,*args):
+        global sc1
+        global x
+        x += 1
+        print(x)
+        Clock.unschedule(self.__init__())
+        #self.manager.current = 'screen1'        
+        
 
 
+sc1 = ScreenOne(name='screen1')
 
-''')
-
-
-NUMBER_OF_BUTTONS = 5
-
-
-class MapScreen(Screen):
-
-    @mainthread
-    def on_enter(self):
-        for i in range(0,NUMBER_OF_BUTTONS):
-            button = Button(text="B_" + str(i))
-            self.ids.grid.add_widget(button)
-
-
-class Test(App):
-
+class TestClass(App):
     def build(self):
-        # Create the screen manager
-        sm = ScreenManager()
-        sm.add_widget(MapScreen(name='main'))
+        global x
+        global sc1
+        
+        
+        sm.add_widget(sc1)
+
         return sm
 
+ 
+        
+if __name__ == "__main__":
+    TestClass().run()
 
-Test().run()
+
+
+    
+    
